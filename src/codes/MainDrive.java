bfb7e80962e022d56ca89b09daf4bc37b93e995a
smin9408 <stmdaks888@naver.com>
2022-02-25 15:45:42 +0900

    중복 숫자가 아니라면, 문제로 선정

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -5,19 +5,36 @@ import java.util.Random;
 public class MainDrive {
 
 	public static void main(String[] args) {
-		
+
 //		컴퓨터가 숫자 출제
 //		int 세 칸짜리 배열
-		
+
 		int[] cpuNumbers = new int[3];
-		
-		for(int i = 0; i < cpuNumbers.length; i++) {
-			while(true) {
+
+		for (int i = 0; i < cpuNumbers.length; i++) {
+			while (true) {
 				Random myRandom = new Random();
-				int randomNum = myRandom.nextInt(9)+1; // 1 ~ 10 직전 (9) 
+				int randomNum = myRandom.nextInt(9) + 1; // 1 ~ 10 직전 (9)
+
+				boolean isDuplOk = true;
+
+				for (int num : cpuNumbers) {
+					if (num == randomNum) {
+						isDuplOk = false;
+						break;
+					}
+				}
+				if (isDuplOk) {
+					cpuNumbers[i] = randomNum;
+					break;
+				}
 			}
-				
+
 		}
 
+//		문제 확인용 for 문
+		for (int num : cpuNumbers) {
+			System.out.println(num);
+		}
 	}
 }
