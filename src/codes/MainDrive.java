d0295c817368635d524ae765ed60d5824016feb9
smin9408 <stmdaks888@naver.com>
2022-02-25 15:58:44 +0900

    S/B 갯수 판단 로직 작성

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -35,7 +35,7 @@ public class MainDrive {
 
 //		문제 확인용 for 문
 		for (int num : cpuNumbers) {
-			System.out.println(num);
+			System.out.print(num);
 		}
 
 //		사용자가 정답을 맞출때 까지 입력받자
@@ -44,8 +44,31 @@ public class MainDrive {
 		while (true) {
 			System.out.println("답안 입력 : ");
 			int inputNum = sc.nextInt();
-			
+
 //			321 => {3, 2, 1} 처럼 세칸 배열로 분리 / 맞춘 S, B 판정.
+
+			int[] myNumbers = new int[3];
+
+			myNumbers[0] = inputNum / 100;
+			myNumbers[1] = (inputNum / 10) % 10;
+			myNumbers[2] = inputNum % 10;
+
+			int strikeCount = 0;
+			int ballCount = 0;
+
+			for (int i = 0; i < myNumbers.length; i++) {
+
+				for (int j = 0; j < cpuNumbers.length; j++) {
+					if (myNumbers[i] == cpuNumbers[j]) {
+						if (i == j) {
+							strikeCount++;
+						}else {
+							ballCount++;
+						}
+					}
+				}
+			}
+			System.out.println(strikeCount + "S, " + ballCount + "B입니다.");
 
 		}
 	}
